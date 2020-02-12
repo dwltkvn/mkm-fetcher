a5a751c428659b19a0ccff4c113a7d868965b519
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2020-02-12 21:40:50 +0100

    add parameters to mkmseller function

--- functions/mkmseller.js
+++ functions/mkmseller.js
@@ -2,32 +2,29 @@ const fetch = require("node-fetch")
 const cheerio = require("cheerio")
 
 const lambda = async (event, context) => {
+  const seller = event.queryStringParameters.seller
+  if (seller === undefined) {
+    return { statusCode: 404, body: "" }
+  }
   const request = await fetch(
-    "https://www.cardmarket.com/en/Magic/Users/Uraktus"
+    "https://www.cardmarket.com/en/Magic/Users/" + seller
   )
   const data = await request.text()
   let $ = cheerio.load(data)
-  let retObject = {}
   let q = 0
-  let u = ""
   $("h3.card-title").each(function(i, elem) {
     //console.log(i);
     //console.log(elem);
     let $quantity = $("span.bracketed", this).text()
-    let seller = "Uraktus"
-    let object = {
-      quantity: $quantity,
-      seller: seller,
-    }
-    retObject[i] = object
     q = $quantity
-    u = seller
   }) // end each()
   //console.log(retObject);
-  const stringified = JSON.stringify({ seller: u, nbcards: q })
+  const stringified = JSON.stringify({ seller: seller, nbcards: q })
   return { statusCode: 200, body: stringified }
 }
 
-//lambda().then(data => console.log(data))
+/*lambda({ queryStringParameters: { seller: "twika" } }).then(data =>
+  console.log(data)
+)*/
 
 exports.handler = lambda
