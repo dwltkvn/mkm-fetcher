4828f2311d5f2c79f925594bb7a1f2c5fe9a1336
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2020-02-13 19:45:13 +0100

    parse card number
    when multiple item on seller page

--- functions/mkmseller.js
+++ functions/mkmseller.js
@@ -12,19 +12,25 @@ const lambda = async (event, context) => {
   const data = await request.text()
   let $ = cheerio.load(data)
   let q = 0
-  $("h3.card-title").each(function(i, elem) {
+  //div > ul > li:nth-child(1) > a > div > h3
+  $(
+    "div > ul > li:nth-child(1) > a > div > h3 > div.bracketed.text-muted.small.mt-1"
+  ).each(function(i, elem) {
+    //$("h3.card-title").each(function(i, elem) {
     //console.log(i);
     //console.log(elem);
-    let $quantity = $("span.bracketed", this).text()
+
+    let $quantity = $(this).text()
     q = $quantity
+    //console.log(i + " : " + q + " --- ")
   }) // end each()
   //console.log(retObject);
   const stringified = JSON.stringify({ seller: seller, nbcards: q })
   return { statusCode: 200, body: stringified }
 }
 
-/*lambda({ queryStringParameters: { seller: "twika" } }).then(data =>
+lambda({ queryStringParameters: { seller: "MagicBazar" } }).then(data =>
   console.log(data)
-)*/
+)
 
 exports.handler = lambda
